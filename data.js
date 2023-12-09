5f00c698a5b51bdfdecd5729de9f9cdfc292692c
Abid Hussain <abidhussain949936@gmail.com>
2023-12-10 00:40:03 +0530

    change

--- data.js
+++ data.js
@@ -17,6 +17,45 @@ const data = {
                {
                     img: "./assests/abid5.jpg",
                },
+               {
+                    img: "./assests/abid6.jpg",
+               },
+               {
+                    img: "./assests/abid7.jpg",
+               },
+               {
+                    img: "./assests/abid8.jpg",
+               },
+               {
+                    img: "./assests/abid9.jpg",
+               },
+               {
+                    img: "./assests/abid10.jpg",
+               },
+               {
+                    img: "./assests/abid11.jpg",
+               },
+               {
+                    img: "./assests/abid12.jpg",
+               },
+               {
+                    img: "./assests/abid13.jpg",
+               },
+               {
+                    img: "./assests/abid14.jpg",
+               },
+               {
+                    img: "./assests/abid15.jpg",
+               },
+               {
+                    img: "./assests/abid16.jpg",
+               },
+               {
+                    img: "./assests/abid17.jpg",
+               },
+               {
+                    img: "./assests/abid18.jpg",
+               },
           ]
      },
      Sazid: {
@@ -109,6 +148,159 @@ const data = {
                     img: "./assests/mujammil19.jpg",
                },
           ]
+     },
+     Rafi: {
+          name: "Rafi Noor",
+          image: [
+               {
+                    img: "./assests/sahil1.jpg",
+               },
+               {
+                    img: "./assests/sahil2.jpg",
+               },
+               {
+                    img: "./assests/sahil3.jpg",
+               },
+               {
+                    img: "./assests/sahil4.jpg",
+               },
+               {
+                    img: "./assests/sahil5.jpg",
+               },
+               {
+                    img: "./assests/sahil6.jpg",
+               },
+               {
+                    img: "./assests/sahil7.jpg",
+               },
+          ]
+     },
+
+     Rizwan: {
+          name: "Rizwan",
+          image: [
+               {
+                    img: "./assests/rizwan2.jpg",
+               },
+               {
+                    img: "./assests/rizwan3.jpg",
+               },
+               {
+                    img: "./assests/rizwan4.jpg",
+               },
+               {
+                    img: "./assests/rizwan5.jpg",
+               },
+               {
+                    img: "./assests/rizwan6.jpg",
+               },
+               {
+                    img: "./assests/rizwan7.jpg",
+               },
+               {
+                    img: "./assests/rizwan8.jpg",
+               },
+               {
+                    img: "./assests/rizwan9.jpg",
+               },
+               {
+                    img: "./assests/rizwan10.jpg",
+               },
+               {
+                    img: "./assests/rizwan11.jpg",
+               },
+               {
+                    img: "./assests/rizwan12.jpg",
+               },
+          ]
+     },
+     
+     Soyab: {
+          name: "Soyab",
+          image: [
+               {
+                    img: "./assests/soyab1.jpg",
+               },
+               {
+                    img: "./assests/soyab2.jpg",
+               },
+               {
+                    img: "./assests/soyab3.jpg",
+               },
+               {
+                    img: "./assests/soyab4.jpg",
+               },
+          ]
+     },
+     
+     Akram: {
+          name: "Akram",
+          image: [
+               {
+                    img: "./assests/akram1.jpg"
+               },
+               {
+                    img: "./assests/akram2.jpg"
+               },
+               {
+                    img: "./assests/akram3.jpg"
+               },
+               {
+                    img: "./assests/akram4.jpg"
+               },
+               {
+                    img: "./assests/akram5.jpg"
+               },
+               {
+                    img: "./assests/akram6.jpg"
+               },
+               {
+                    img: "./assests/akram7.jpg"
+               },
+               {
+                    img: "./assests/akram8.jpg"
+               },
+               {
+                    img: "./assests/akram9.jpg"
+               },
+               {
+                    img: "./assests/akram10.jpg"
+               },
+          ]
+     },
+     Ikram: {
+          name: "Ikram",
+          image: [
+               {
+                    img: "./assests/ikram1.jpg"
+               },
+               {
+                    img: "./assests/ikram2.jpg"
+               },
+               {
+                    img: "./assests/ikram3.jpg"
+               },
+               {
+                    img: "./assests/ikram4.jpg"
+               },
+               {
+                    img: "./assests/ikram5.jpg"
+               },
+          ]
+     },
+     Junaid: {
+          name: "Junaid",
+          image:[
+               {
+                    img: "./assests/junaid1.jpg"
+               },
+               {
+                    img: "./assests/junaid2.jpg"
+               },
+               {
+                    img: "./assests/junaid3.jpg"
+               },
+          ]
      }
 };
 
@@ -119,8 +311,10 @@ let h2 = document.querySelector("h2");
 
 submit.addEventListener("click", function () {
      let name = (inpt.value).trim();
-     if ("Abid" == name) {
-          for (let i = 0; i < 5; i++) {
+     let orgName = name.toUpperCase();
+     console.log(orgName);
+     if ("ABID" == orgName) {
+          for (let i = 0; i < 18; i++) {
                let imag = document.createElement("img");
                let images = document.body.appendChild(imag);
                images.setAttribute("class", "images");
@@ -128,7 +322,7 @@ submit.addEventListener("click", function () {
           };
      };
 
-     if ("Sazid" == name) {
+     if ("SAZID" == orgName) {
           for (let i = 0; i < 5; i++) {
                let imag = document.createElement("img");
                let images = document.body.appendChild(imag);
@@ -138,12 +332,68 @@ submit.addEventListener("click", function () {
           };
      };
 
-     if ("Mujammil" == name) {
+     if ("MUJAMMIL" == orgName) {
           for (let i = 0; i < 20; i++) {
                let imag = document.createElement("img");
                let images = document.body.appendChild(imag);
                images.setAttribute("class", "images");
                images.src = data.Mujammil.image[i].img;
+          };
+     };
+
+     if ("RAFI NOOR" == orgName) {
+          for (let i = 0; i < 7; i++) {
+               let imag = document.createElement("img");
+               let images = document.body.appendChild(imag);
+               images.setAttribute("class", "images");
+               images.src = data.Rafi.image[i].img;
+
+          };
+     };
+
+     if ("RIZWAN" == orgName) {
+          for (let i = 0; i < 11; i++) {
+               let imag = document.createElement("img");
+               let images = document.body.appendChild(imag);
+               images.setAttribute("class", "images");
+               images.src = data.Rizwan.image[i].img;
+
+          };
+     };
+
+     if ("SOYAB" == orgName) {
+          for (let i = 0; i < 4; i++) {
+               let imag = document.createElement("img");
+               let images = document.body.appendChild(imag);
+               images.setAttribute("class", "images");
+               images.src = data.Soyab.image[i].img;
+          };
+     };
+
+     if ("AKRAM" == orgName) {
+          for (let i = 0; i < 10; i++) {
+               let imag = document.createElement("img");
+               let images = document.body.appendChild(imag);
+               images.setAttribute("class", "images");
+               images.src = data.Akram.image[i].img;
+          };
+     };
+
+     if ("IKRAM" == orgName) {
+          for (let i = 0; i < 5; i++) {
+               let imag = document.createElement("img");
+               let images = document.body.appendChild(imag);
+               images.setAttribute("class", "images");
+               images.src = data.Ikram.image[i].img;
+          };
+     };
+
+     if ("JUNAID" == orgName) {
+          for (let i = 0; i < 3; i++) {
+               let imag = document.createElement("img");
+               let images = document.body.appendChild(imag);
+               images.setAttribute("class", "images");
+               images.src = data.Junaid.image[i].img;
           };
      };
      inpt.value = "";
